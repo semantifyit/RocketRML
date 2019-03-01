c3c7506e986d3a655394da6839f9bebb65e1facc
Philipp Häusle <philipp.haeusle@hotmail.com>
2019-03-01 13:27:03 +0100

    add outputs

--- tests/RMLio-testCases/rmlio-tests.js
+++ tests/RMLio-testCases/rmlio-tests.js
@@ -2,7 +2,7 @@ const testFolder = './tests/RMLio-testCases';
 const parser = require('../../index.js');
 const fs = require('fs');
 
-/*fs.readdirSync(testFolder).forEach(async file => {
+fs.readdirSync(testFolder).forEach(async file => {
     let options={};
     let ttlPAth='./tests/RMLio-testCases/'+file+'/mapping.ttl';
     let mapfile=fs.readFileSync(ttlPAth,'utf8');
@@ -15,13 +15,4 @@ const fs = require('fs');
         console.log(err);
     });
     console.log(result);
-});*/
-
-let testSingle = async (dir,options) => {
-    let result = await parser.parseFile('./tests/RMLio-testCases/' + dir + '/mapping.ttl', './tests/RMLio-testCases/' + dir + '/out.json', options).catch((err) => {
-        console.log(err);
-    });
-    console.log(result);
-};
-
-testSingle('RMLTC0020a-XML');
+});
